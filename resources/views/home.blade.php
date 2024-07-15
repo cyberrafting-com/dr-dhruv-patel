c0821252804aced4bd7519db9a9bc8c063a68775
Ganesh Pardhe <ganesh.pardhe@cyberrafting.com>
2024-07-15 16:11:03 +0530

    remove comment section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
 @section('meta_keywords')Best Orthopedic doctor in Navi mumbai, Best Orthopaedic doctor in Navi Mumbai, best joint replacement doctor in Navi Mumbai, Best arthroscopy doctor in Navi Mumbai, best arthroscopic doctor in Navi Mumbai, best Trauma and Fracture surgery doctor in Navi Mumbai, best spine surgery doctor in Navi Mumbai, best joint pain doctor in Navi Mumbai, best arthritis doctor in navi mumbai, best sport injury doctor in navi mumbai, best sport injuries doctor in navi mumbai, Best Orthopedic surgeon in Navi mumbai, Best Orthopaedic surgeon in Navi Mumbai, best joint replacement surgeon in Navi Mumbai, Best arthroscopy surgeon in Navi Mumbai, best arthroscopic surgeon in Navi Mumbai, best Trauma and Fracture surgery surgeon in Navi Mumbai, best spine surgery surgeon in Navi Mumbai, best joint pain surgeon in Navi Mumbai, best arthritis surgeon in navi mumbai, best sport injury surgeon in navi mumbai, best sport injuries surgeon in navi mumbai,
 Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint replacement doctor in Vashi, Best arthroscopy doctor in Vashi, best arthroscopic doctor in Vashi, best Trauma and Fracture surgery doctor in Vashi, best spine surgery doctor in Vashi, best joint pain doctor in Vashi, best arthritis doctor in Vashi, best sport injury doctor in Vashi, best sport injuries doctor in Vashi, Best Orthopedic surgeon in Vashi, Best Orthopaedic surgeon in Vashi, best joint replacement surgeon in Vashi, Best arthroscopy surgeon in Vashi, best arthroscopic surgeon in Vashi, best Trauma and Fracture surgery surgeon in Vashi, best spine surgery surgeon in Vashi, best joint pain surgeon in Vashi, best arthritis surgeon in Vashi, best sport injury surgeon in Vashi, best sport injuries surgeon in Vashi, Best Orthopedic doctor in Belapur, Best Orthopaedic doctor in Belapur, best joint replacement doctor in Belapur, Best arthroscopy doctor in Belapur, best arthroscopic doctor in Belapur, best Trauma and Fracture surgery doctor in Belapur, best spine surgery doctor in Belapur, best joint pain doctor in Belapur, best arthritis doctor in Belapur, best sport injury doctor in Belapur, best sport injuries doctor in Belapur, Best Orthopedic surgeon in Belapur, Best Orthopaedic surgeon in Belapur, best joint replacement surgeon in Belapur, Best arthroscopy surgeon in Belapur, best arthroscopic surgeon in Belapur, best Trauma and Fracture surgery surgeon in Belapur, best spine surgery surgeon in Belapur, best joint pain surgeon in Belapur, best arthritis surgeon in Belapur, best sport injury surgeon in Belapur, best sport injuries surgeon in Belapur @endsection 
 @section('meta_tags')
+
+
+
     <meta name="keywords" content="@yield('meta_keywords')">
   <meta name="description" content="@yield('meta_desc')">
 @endsection
@@ -39,16 +42,6 @@ Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint re
 </div>
 
  
-        <!--<section class="banner-set">-->
-        <!--    <div class="slider-container">-->
-        <!--        <img src="{{asset('/resources/assets/images/main-slider/dr-dhruv-patel-banner-1.jpg') }}"  alt="Image 1">-->
-        <!--        <img src="{{ asset('/resources/assets/images/main-slider/dr-dhruv-patel-banner-2.jpg') }}" alt="Image 2">-->
-            
-        <!--    </div>-->
-        <!--</section>-->
-        
-      
-        
         <!--Welcome Section-->
         <section class="welcome-section">
             <div class="container">
@@ -94,97 +87,7 @@ Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint re
         <!--End Welcome Section-->
 
         <!--Services Section-->
-        <!-- <section class="services-section">
-            <div class="auto-container">
-         
-                <div class="sec-title">
-                    <h2>Treatment We Offer</h2>
-                  
-                </div>
-                <div class="four-item-carousel owl-carousel owl-theme">
-
-              
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/joint-replacement.png') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Joint Replacement<br>Surgery</a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-           
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/arthroscopy-surgery.jpg') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Arthroscopy<br>Surgery</a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
        
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/traum-fracture-surgery.png') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Trauma And Fracture<br>Surgery </a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-               
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/spine-surgery.png') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Spine<br>Surgery</a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/joint-pain-clinic.png') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Joint Pain<br>Clinic </a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                   
-                    <div class="services-block">
-                        <div class="inner-box">
-                            <div class="image">
-                                <img src="{{ asset('/resources/assets/images/treatments/pain-management-clinic.jpg') }}" alt="" />
-                                <div class="overlay"></div>
-                                <div class="content">
-                                    <h3><a href="#">Pain Management<br>Clinic</a></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section> -->
         <!--End Services Section-->
 
 
@@ -377,15 +280,6 @@ Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint re
                         </a>
                     </div>
                 </div>
-              
-                
-                
-               
-                
-                 
-              
-
-
             </div>
         </div>
 <!--Clinic images End-->
@@ -402,18 +296,7 @@ Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint re
 
                 </div>
 
-                    <!-- <div class="col-md-4">
-                            <div class="map-set">
-                                <i class="fa fa-hospital-o" aria-hidden="true"></i>
-
-                                <h2>ARTI POLYCLINIC</h2>
-
-                                <p>1st Floor, Aarti Apartment, Sector - 2, Above Ratnakar Bank, Vashi, Navi Mumbai - 400705, Sector 2, Vashi, Navi Mumbai, Maharashtra 400703</p>
-                                <p class="text-center map-p">022 27825859 </p>
-
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.763666923305!2d72.99492817438174!3d19.07412575203296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c14a7bffffff%3A0x3f96120a42d2de1a!2sArti%20Polyclinic!5e0!3m2!1sen!2sin!4v1683722200564!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
-                        </div> -->
+                  
 
                     <div class="col-md-4">
 
@@ -423,16 +306,13 @@ Best Orthopedic doctor in Vashi, Best Orthopaedic doctor in Vashi, best joint re
 
                                 <!-- <h5>ORTHOPEDIC DEPARTMENT</h5> -->
                                 <div class="image consult">
-                                <img src="{{ asset('/resources/assets/images/icons/shree-logo .jpg') }}" alt="SHREE HOSPITAL AND ICU,
-NAVI MUMBAI">
+                    <img src="{{ asset('/resources/assets/images/icons/shree-logo .jpg') }}" alt="SHREE HOSPITAL AND ICU,NAVI MUMBAI">
 
                             </div>
                                 <h3>SHREE HOSPITAL AND ICU, <br> NAVI MUMBAI </h3>
                                 <i class="fa-sharp fa-solid fa-location-dot"></i>
                                 <p>2nd Floor, Raikar Bhavan Nr. Navaratna Hotel, Pedestrian Path, Sector 17, Vashi, Navi Mumbai, Maharashtra 400703</p>
-                                <!-- <p class="text-center map-p">09820229691 </p>
-
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1845.5382271360454!2d73.22356750180673!3d22.31294812060965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fcf795d9b8a0f%3A0x13762a813487c6d6!2sShree%20Hospital%20%26%20ICU!5e0!3m2!1sen!2sin!4v1683784078188!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                              
                             </div>
                         </div>
                            <div class="col-md-4">
@@ -448,9 +328,7 @@ NAVI MUMBAI">
 
                                 <i class="fa-sharp fa-solid fa-location-dot"></i>
                                 <p>2nd Floor, Om Chanakya Complex, Sector 6, CBD Belapur, Navi Mumbai, Maharashtra 400614</p>
-                                <!-- <p class="text-center map-p">022 27552000</p>
-
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.056978339899!2d73.00193097438249!3d19.10515615105823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c0d0851e2aa3%3A0xa147281cb5a51981!2sMathadi%20Hospital%20Trust!5e0!3m2!1sen!2sin!4v1683784980280!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                             
                             </div>
                         </div>
 
@@ -460,8 +338,6 @@ NAVI MUMBAI">
                             <div class="image consult">
                                 <img src="{{ asset('/resources/assets/images/icons/MPCT-HOSPITAL.png') }}" alt="MPCT HOSPITAL, NAVI MUMBAI">
                             </div>
-                   
-
                                 <!-- <h5>HIP AND KNEE REPLACEMENT SURGEON</h5> -->
 
                                 <h3>MPCT HOSPITAL, NAVI MUMBAI</h3>
